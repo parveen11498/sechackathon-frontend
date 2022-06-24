536e707e6ef5b6e955382c66fe63044921abef5d
parvin khan <parveen4900613@gmail.com>
2022-06-24 16:11:47 +0530

    first commit

---
+++ src/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import {
+//   ul
+// //   NavLink,
+// //   Bars,
+// //   NavMenu,
+// //   NavBtn,
+// //   NavBtnLink,
+// // } from './NavbarElements';
+// }
+const Navbar = () => {
+return(
+   
+  <ul>
+  <li><a href="Login">Home</a></li>
+  <li><a href="Login">News</a></li>
+  <li><a href="Login">Contact</a></li>
+  <li><a href="Login">About</a></li>
+</ul> 
+)
+
+   
+  };
+  
+export default Navbar;
